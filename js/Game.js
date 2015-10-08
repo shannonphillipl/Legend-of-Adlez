97d982734c2894e7b440e8d3c1d4ce6325b9633f
Alex Costa <alexmcosta.dev@gmail.com>
2015-10-08 11:28:34 -0700

    add music points in code

--- js/Game.js
+++ js/Game.js
@@ -3,9 +3,15 @@ var TopDownGame = TopDownGame || {};
 //title screen
 TopDownGame.Game = function(){};
 
+var music;
+
 TopDownGame.Game.prototype = {
   create: function() {
     this.map = this.game.add.tilemap('level1');
+
+    //Add music
+    music = game.add.audio('adlezSong');
+    music.onDecoded.add(start, this);
 
     //First argument: the tileset name as specified in Tiled; Second argument: the key to the asset
     this.map.addTilesetImage('tiles', 'gameTiles');
@@ -162,7 +168,7 @@ TopDownGame.Game.prototype = {
     });
     return result;
   },
- 
+
 
   //fire bullet
   fireBullet: function() {
--- js/Preload.js
+++ js/Preload.js
@@ -22,6 +22,7 @@ TopDownGame.Preload.prototype = {
     this.load.spritesheet('chicken', 'assets/images/chicken.png', 32, 32);
     this.game.load.spritesheet('kaboom', 'assets/images/explosion.png', 64, 64);
     this.load.spritesheet('goonUp', 'assets/images/goonUp.png', 32, 32);
+    // this.load.audio('adlezSong', 'assets/audio/adlezSong.ogg');
   },
   create: function() {
     this.state.start('Game');
