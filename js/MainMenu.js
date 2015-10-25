cdc7a29ec58e3568d9c93b2c5adbaa598df7a2a6
Phillip Shannon <shannonphillipl@gmail.com>
2015-10-25 02:30:00 -0700

    Add new logo and load bar

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -7,12 +7,16 @@ LegendOfAdlez.MainMenu.prototype = {
   create: function() {
     //show map scrolling in background
     this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'map');
-    this.background.autoScroll(-1170, 0);
+    this.background.autoScroll(-20, -20);
 
     //Start game
+    //logo
+    this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+    this.splash.anchor.setTo(0.5);
+
     var text = "Press SPACE to begin";
     var style = { font: "30px Arial", fill: "#fff", align: "center" };
-    var t = this.game.add.text(this.game.width/2, this.game.height/2, text, style);
+    var t = this.game.add.text(this.game.width/2, (this.game.height/2)+75, text, style);
     t.anchor.set(0.5);
 
     this.startButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
